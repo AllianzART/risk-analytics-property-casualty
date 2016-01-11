9e3f12378a63d071986b5cf662821ecf49b75f50
fazl <art.frahman@gmail.com>
2016-01-11 09:21:28 +0100

    [AR-208] Build pc plugin v2.0.0 for release

--- RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
+++ RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import org.pillarone.riskanalytics.core.util.ResourceBundleRegistry
 
 class RiskAnalyticsPropertyCasualtyGrailsPlugin {
     // the plugin version
-    def version = "2.0.0-SNAPSHOT"
+    def version = "2.0.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3.2 > *"
     // the other plugins this plugin depends on
